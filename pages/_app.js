fa65895addad84668fe956e112d37a94dde307da
LeKZanGx <lekkymc123@gmail.com>
2023-01-19 12:05:04 +0700

    Update.......

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/style.css';
 import '../styles/scss/custom.css'
 import 'animate.css'
-import '../styles/mdiicons.min.css'
 import { NavbarComponent } from "../components/Navbar";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
@@ -21,6 +20,7 @@ export default function App({
         <title>SwitchHub | Shop</title>
         <link rel="icon" href="/favicon.ico" />
         <meta httpEquiv="Content-Security-Policy" content=""></meta>
+        <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/@mdi/font@6.9.96/css/materialdesignicons.min.css"/>
       </Head>
       <NavbarComponent />
       <Component {...pageProps} />
