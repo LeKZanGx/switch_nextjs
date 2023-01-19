13cb69ef02f9385b002bf9182490c69a602b9886
LeKZanGx <lekkymc123@gmail.com>
2023-01-19 13:44:51 +0700

    StyleText

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,14 @@ export default function Home() {
       <br></br>
       <br></br>
       <br></br>
+      <br></br>
+      <br></br>
       <h1 className="linear-wipe">Switch Hub</h1>
-      <h3 className="text-white home-text mt-4">- Next Generation Feature-</h3>
-      <h5 className="text-white home-text mt-4">#1 Quality Script</h5>
-      <h5 className="text-white home-text">ค่ายก่ำๆ สคริปกังๆ ยกให้ SwitchHub</h5>
-      <h5 className="text-white home-text">ค่ายก่ำๆ สคริปกังๆ ยกให้ SwitchHub</h5>
-      <h5 className="text-white home-text">ค่ายก่ำๆ สคริปกังๆ ยกให้ SwitchHub</h5>
+      <h2 className="text-white home-text mt-4">- Next Generation Feature-</h2>
+      <h3 className="text-white home-text mt-4">#1 Quality Script</h3>
+      <h3 className="text-white home-text">ค่ายก่ำๆ สคริปกังๆ ยกให้ SwitchHub</h3>
+      <h3 className="text-white home-text">ค่ายก่ำๆ สคริปกังๆ ยกให้ SwitchHub</h3>
+      <h3 className="text-white home-text">ค่ายก่ำๆ สคริปกังๆ ยกให้ SwitchHub</h3>
     </div>
   );
 }
