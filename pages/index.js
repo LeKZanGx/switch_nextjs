5a04b3146bb83376de6f43adcf1187134ef1a6cf
LeKZanGx <lekkymc123@gmail.com>
2023-01-19 12:27:56 +0700

    Update Index555

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,17 @@
-import { Card, Col, Container, Row } from "react-bootstrap";
-
+import Image from "next/image";
 export default function Home() {
   return (
     <div className="container-fluid banner-home p-4 pt-5 pb-5 m-cent">
       <div className="container p-4 pt-5 pb-5 m-cent">
         <div className="row justify-content-center justify-content-lg-between">
           <div className="col-lg mb-4">
-
+            <Image
+              src=""
+              alt="banner-switchhub"
+            />
           </div>
           <div className="col-lg mb-4">
-          
+            <h3 className="linear-wipe">SwitchHub | No. 1</h3>
           </div>
         </div>
       </div>
