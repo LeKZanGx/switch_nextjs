0adf569d8475aeac70ad20e205861d5eb2262386
LeKZanGx <lekkymc123@gmail.com>
2023-01-19 12:51:28 +0700

    Update HOME

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,13 @@ export default function Home() {
     <div className="container-fluid banner-home p-4 pt-5 pb-5 m-cent">
       <div className="container p-4 pt-5 pb-5 m-cent">
         <div className="row justify-content-center justify-content-lg-between">
-          <div className="col-lg mb-4">
-            <Image
-              src=""
-              alt="banner-switchhub"
-            />
+          <div className="d-flex justify-content-center">
+            <div className="col-lg mb-4">
+              <Image
+                src="https://cdn.discordapp.com/icons/961866856283635732/1aa2c24f001a61d7def13c9aa460b18c.webp?size=512"
+                alt="banner-switchhub"
+              />
+            </div>
           </div>
           <div className="col-lg mb-4">
             <h2 className="text-center linear-wipe">SwitchHub</h2>
