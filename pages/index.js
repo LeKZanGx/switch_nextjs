2cd1e81236fcb4adc29fa3c4f89138ff61713aca
LeKZanGx <lekkymc123@gmail.com>
2023-01-19 13:03:19 +0700

    Update photo position

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,15 @@ export default function Home() {
     <div className="container-fluid banner-home p-4 pt-5 pb-5 m-cent">
       <div className="container p-4 pt-5 pb-5 m-cent">
         <div className="row justify-content-center justify-content-lg-between">
-          <div className="col-lg mb-4 justify-content-center text-center">
-            <Image
-              src="https://cdn.discordapp.com/icons/961866856283635732/1aa2c24f001a61d7def13c9aa460b18c.webp?size=512"
-              width="400"
-              height="400"
-              alt="banner-switchhub"
-            />
+          <div className="justify-content-center">
+            <div className="col-lg mb-4 text-center">
+              <Image
+                src="https://cdn.discordapp.com/icons/961866856283635732/1aa2c24f001a61d7def13c9aa460b18c.webp?size=512"
+                width="350"
+                height="350"
+                alt="banner-switchhub"
+              />
+            </div>
           </div>
           <div className="col-lg mb-4">
             <h2 className="text-center linear-wipe">SwitchHub</h2>
