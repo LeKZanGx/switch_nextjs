6f854a033bd47f0ceb91102d7de6eaf2f9c99949
LeKZanGx <lekkymc123@gmail.com>
2023-01-19 13:37:13 +0700

    Update Fonts

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,8 @@ export default function Home() {
       <br></br>
       <br></br>
       <h1 className="linear-wipe">Switch Hub</h1>
-      <h3 className="text-white home-text mt-2">- Next Generation Feature-</h3>
-      <h5 className="text-white home-text mt-2">#1 Quality Script</h5>
+      <h3 className="text-white home-text mt-4">- Next Generation Feature-</h3>
+      <h5 className="text-white home-text mt-4">#1 Quality Script</h5>
       <h5 className="text-white home-text">ค่ายก่ำๆ สคริปกังๆ ยกให้ SwitchHub</h5>
       <h5 className="text-white home-text">ค่ายก่ำๆ สคริปกังๆ ยกให้ SwitchHub</h5>
       <h5 className="text-white home-text">ค่ายก่ำๆ สคริปกังๆ ยกให้ SwitchHub</h5>
