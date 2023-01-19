fcbe56aef550dd18ff6a0aed7b427a3416e784f0
LeKZanGx <lekkymc123@gmail.com>
2023-01-19 12:13:08 +0700

    Update HomePage1

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,17 @@ import { Card, Col, Container, Row } from "react-bootstrap";
 
 export default function Home() {
   return (
-    <Container>
-      <Row className="justify-content-md-center">
-        <Col md={12}>
-          <div className="bg-gradient-to-b from-xenon to-xenon/50 roundedfew pt-36 py-72">
+    <div className="container-fluid banner-home p-4 pt-5 pb-5 m-cent">
+      <div className="container p-4 pt-5 pb-5 m-cent">
+        <div className="row justify-content-center justify-content-lg-between">
+          <div className="col-lg mb-4">
 
           </div>
-          <Card bg='dark' key='dark' text='light' border='success'>
-            <Card.Body>
-              <Card.Body>
-                <Row>
-                </Row>
-              </Card.Body>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+          <div className="col-lg mb-4">
+          
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
