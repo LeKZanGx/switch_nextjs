194be46aa1e37fe792be2f37de58494739bb816e
LeKZanGx <lekkymc123@gmail.com>
2023-01-25 22:00:17 +0700

    fix two

--- pages/history/stock.js
+++ pages/history/stock.js
@@ -8,7 +8,7 @@ function historyStock() {
     const [history, setHistory] = useState(null);
     const current = new Date();
     const date = `${current.getDate()}/${current.getMonth() + 1}/${current.getFullYear()} ${current.getHours()}:${current.getMinutes()}:${current.getSeconds()}`;
-    useEffect(() => {
+    useEffect(function persistLoad(){
         async function fetch() {
             const data = {
                 username: localStorage.getItem("username")
--- pages/history/topup.js
+++ pages/history/topup.js
@@ -8,7 +8,7 @@ function historyTopup() {
     const current = new Date();
     const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()} ${current.getHours()}:${current.getMinutes()}:${current.getSeconds()}`;
     const [topuphistory, setTopupHistory] = useState(null);
-    useEffect(() => {
+    useEffect(function persistLoad() {
         async function fetch() {
             const data = {
                 username: localStorage.getItem("username")
